cf555a9bdc2058108f87224a81b5fe4550f8f4ec
yarabramasta <yarabram111@gmail.com>
2024-09-17 19:02:39 +0700

    chore: clean up & merge sql migrations

--- drizzle/migrations/0001_rapid_vulture.sql
+++ drizzle/migrations/0001_rapid_vulture.sql
@@ -10,20 +10,21 @@ CREATE TABLE IF NOT EXISTS "flags" (
 	CONSTRAINT "flags_code_unique" UNIQUE("code")
 );
 --> statement-breakpoint
-CREATE TABLE IF NOT EXISTS "lingua"."glossaries" (
+CREATE TABLE IF NOT EXISTS "lingua"."lairs" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"user_id" text NOT NULL,
 	"slug" text NOT NULL,
 	"name" text NOT NULL,
 	"description" text NOT NULL,
+	"primary" boolean DEFAULT false NOT NULL,
 	"created_at" timestamp (3) with time zone DEFAULT now(),
 	"updated_at" timestamp (3) with time zone DEFAULT now(),
-	CONSTRAINT "glossaries_slug_unique" UNIQUE("slug")
+	CONSTRAINT "lairs_slug_unique" UNIQUE("slug")
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "lingua"."languages" (
 	"id" serial PRIMARY KEY NOT NULL,
-	"glossary_id" bigint NOT NULL,
+	"lair_id" bigint NOT NULL,
 	"code" text NOT NULL,
 	"name" text NOT NULL,
 	"description" text NOT NULL,
@@ -51,7 +52,6 @@ CREATE TABLE IF NOT EXISTS "lingua"."tokens" (
 CREATE TABLE IF NOT EXISTS "auth"."users" (
 	"id" text PRIMARY KEY NOT NULL,
 	"username" text NOT NULL,
-	"avatar" text NOT NULL,
 	"password_hash" text NOT NULL,
 	"created_at" timestamp (3) with time zone DEFAULT now() NOT NULL,
 	"updated_at" timestamp (3) with time zone DEFAULT now() NOT NULL,
@@ -71,13 +71,13 @@ CREATE TABLE IF NOT EXISTS "lingua"."versions" (
 );
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "lingua"."glossaries" ADD CONSTRAINT "glossaries_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "auth"."users"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "lingua"."lairs" ADD CONSTRAINT "lairs_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "auth"."users"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "lingua"."languages" ADD CONSTRAINT "languages_glossary_id_glossaries_id_fk" FOREIGN KEY ("glossary_id") REFERENCES "lingua"."glossaries"("id") ON DELETE cascade ON UPDATE no action;
+ ALTER TABLE "lingua"."languages" ADD CONSTRAINT "languages_lair_id_lairs_id_fk" FOREIGN KEY ("lair_id") REFERENCES "lingua"."lairs"("id") ON DELETE cascade ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
@@ -107,12 +107,12 @@ EXCEPTION
 END $$;
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "lingua"."versions" ADD CONSTRAINT "versions_previous_version_id_versions_id_fk" FOREIGN KEY ("previous_version_id") REFERENCES "lingua"."versions"("id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "lingua"."versions" ADD CONSTRAINT "versions_previous_version_id_versions_id_fk" FOREIGN KEY ("previous_version_id") REFERENCES "lingua"."versions"("id") ON DELETE set null ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
 --> statement-breakpoint
-CREATE INDEX IF NOT EXISTS "glossaries_name_index" ON "lingua"."glossaries" USING btree ("name");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "lairs_name_index" ON "lingua"."lairs" USING btree ("name");--> statement-breakpoint
 CREATE INDEX IF NOT EXISTS "languages_code_index" ON "lingua"."languages" USING btree ("code");--> statement-breakpoint
 CREATE INDEX IF NOT EXISTS "languages_name_index" ON "lingua"."languages" USING btree ("name");--> statement-breakpoint
 CREATE INDEX IF NOT EXISTS "tokens_key_index" ON "lingua"."tokens" USING btree ("key");--> statement-breakpoint
--- drizzle/migrations/0003_left_owl.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE "lingua"."versions" DROP CONSTRAINT "versions_previous_version_id_versions_id_fk";
---> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "lingua"."versions" ADD CONSTRAINT "versions_previous_version_id_versions_id_fk" FOREIGN KEY ("previous_version_id") REFERENCES "lingua"."versions"("id") ON DELETE set null ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
